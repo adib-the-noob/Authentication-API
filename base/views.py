6a0f83c314a03b8fb9e6188f7d7e65add8099d48
ADIB-the-NOOB <mdadib550@gmail.com>
2022-10-08 01:36:58 +0600

    Custom JSONRenderer

--- base/views.py
+++ base/views.py
@@ -9,12 +9,13 @@ from django.contrib.auth import authenticate
 class UserRegistration(APIView):
     def post(self,request,format=None):
         serializer = UserSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
+        if serializer.is_valid():
             serializer.save()
             return Response(
                 {'message':serializer.data},
                 status=status.HTTP_201_CREATED
                 )
+        print(serializer.errors)
         return Response(
             serializer.errors,
             status=status.HTTP_400_BAD_REQUEST
@@ -33,5 +34,5 @@ class UserLoginView(APIView):
                     status=status.HTTP_200_OK
                     )
             else:
-                return Response({'error':{'non_field_errors' : ['Email or Password is not Valid!']}},status=status.HTTP_404_NOT_FOUND)
+                return Response({'error':{'non_field_errors' : ['Email or Password is not Valid!']}},status=status.HTTP_4)
         return Response({'message':'Login'})
