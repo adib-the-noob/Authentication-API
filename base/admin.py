fc6655682f92649a6fa7b643fd91ef6638f366ac
ADIB-the-NOOB <mdadib550@gmail.com>
2022-10-07 19:34:16 +0600

    Custom Model Admin

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+
+
+class UserModelAdmin(BaseUserAdmin):
+    # The forms to add and change user instances
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ('email', 'name','tc', 'is_admin')
+    list_filter = ('is_admin',)
+    fieldsets = (
+        ("UserCredentials", {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('name','tc')}),
+        ('Permissions', {'fields': ('is_admin',)}),
+    )
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name','tc', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('email',)
+    ordering = ('email','id')
+    filter_horizontal = ()
+
+
+# Now register the new UserAdmin...
+admin.site.register(User, UserModelAdmin)
